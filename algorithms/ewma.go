f946dae1750a120d593e67003e7c048a45e03ebc
aasimkhan02 <aasimkroc@gmail.com>
2025-12-18 21:22:56 +0530

    added ewma

--- algorithms/ewma.go
+++ algorithms/ewma.go
@@ -1,7 +1,37 @@
 package main
 
-type evwa struct {
-
+type EWMA struct {
+	alpha float64
+	value float64
+	prev float64
+	initialized bool
 }
 
-//EWMA_new = α * current_rate + (1 - α) * EWMA_old
+func NewEWMA(alpha float64) *EWMA {
+	return &EWMA{
+		alpha: alpha,
+	}
+}
+
+func (e *EWMA) Update(current_rate float64) {
+	if !e.initialized {
+		e.value = currentRate
+		e.prev = currentRate
+		e.initialized = true
+		return
+	}
+
+	e.prev = e.value
+	e.value = e.alpha * currentRate + (1.0 - e.alpha) * e.value
+}
+
+func (e *EWMA) Derivative() float64 {
+	if !e.initialized {
+		return 0
+	}
+	return e.value - e.prev
+}
+
+func (e *EWMA) Value() float64 {
+	return e.value
+}
